81038e94833424a904d93a7256a12cd7212e0fc7
Cloud9June <83631019+Cloud9June@users.noreply.github.com>
2025-09-24 09:24:31 +0900

    Add files via upload

---
+++ stu/service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "sungilnow-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// 설치 단계 (앱 처음 실행 시 캐싱)
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// 네트워크 요청 가로채기
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
